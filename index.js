0705e8db2d134adc12cb4d14c59d0a8dbcb3ed5d
Cristian Alvarado <codigoworks@gmail.com>
2025-06-15 22:09:17 -0400

    'boost'

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import morgan from "morgan";
 import http from 'http';
 import dotenv from 'dotenv';
-import { neon } from '@neondatabase/serverless';
 import loginRoutes from './src/routes/login.routes.js';
 import logger from './src/middlewares/logReq.js';
 import {errorHandler as error} from './src/middlewares/errorHandler.js';
@@ -21,7 +20,6 @@ app.use(tokenAuth);
 
 // app.use(logger);//para activar el log de las consultas a la API
 
-const sql =  neon(process.env.DATABASE_URL);
 //ROUTES
 app.use(loginRoutes);
 
@@ -29,12 +27,12 @@ app.get('/login',async(req,res)=>{
   // const sql =  neon(process.env.DATABASE_URL);
     // res.send('Hola mundo');
   // const result = await sql(`SELECT * from security.user`);
-  const result = await sql(`SELECT * from security.pr_crud_user(0,'n88','e88','p','s','rt')`);
-  console.log('login',result);
+  // const result = await sql(`SELECT * from security.pr_crud_user(0,'n88','e88','p','s','rt')`);
+  console.log('login');
   
   // const { version } = result[0];
   // res.writeHead(200, { "Content-Type": "text/plain" });
-  res.send(result);
+  res.send('login ok');
 })
 
 app.get('/message',(req,res)=>{
--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,34 +1,29 @@
-import {consulta as da} from '../connection/connexPostgres.js'
+import { consulta as da } from "../connection/connexPostgres.js";
 
 /************Generador Dinamico de CRUDS Postgres */
-export const generarCRUDPostgres = function (datos, respuesta) {
-  console.log('generarCRUDPostgres',datos.query);
-  
-  const { opcion,base,esquema,tabla } = datos.query;
-  let q =``;
-  if(opcion=='POSTGRES') q = `SELECT column_name campo, data_type tipo FROM information_schema.columns
+export const generarCRUDPostgres = async function (req, res, next) {
+  console.log("generarCRUDPostgres", req.query);
+
+  const { opcion, esquema, tabla } = req.query;
+  let q = ``;
+  if (opcion == "POSTGRES")
+    q = `SELECT column_name campo, data_type tipo FROM information_schema.columns
   WHERE table_schema = '${esquema}' AND table_name = '${tabla}' order by ordinal_position;`;
   console.log(q);
-
-  const cnx = (base == 'seguimiento') ? connseguimiento : conn;
-
-  cnx.obtener(function (er, cn) {
-      cn.query(q, async function (error, resultado) {
-          cn.release();
-          if (error) respuesta.send([{ message: "Error: " + error }]);
-          if (!error){
-              const data = await armarCRUD(resultado.rows,esquema,tabla,cnx)
-              console.log('ya casi',data.message);
-              respuesta.send(data);
-          }
-    });
-  });
+  try {
+    const consulta = await da(q);
+    let data = "";
+    if (consulta.length > 0) data = await armarCRUD(consulta, esquema, tabla);
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const armarCRUD =(campos,esquema,tabla,cnx)=>{
-  return new Promise((resolve, reject) => {
-      let armado = {}
-      let plantilla = `
+const armarCRUD = (campos, esquema, tabla) => {
+  return new Promise(async (resolve, reject) => {
+    let armado = {};
+    let plantilla = `
 
       CREATE FUNCTION ${esquema}.pra_crud_${tabla}(operacion varchar,#CAMPOS_IN# _usuario_registro int, OUT message varchar)
       RETURNS character varying
@@ -40,7 +35,9 @@ const armarCRUD =(campos,esquema,tabla,cnx)=>{
           BEGIN
               if (operacion='I') then
                   insert into ${esquema}.${tabla}(#CAMPOS_INSERT#)
-                  values (#VALORES_INSERT# now()::timestamp, _usuario_registro,null,null,1) RETURNING ${campos[0].campo} INTO id;  
+                  values (#VALORES_INSERT# now()::timestamp, _usuario_registro,null,null,1) RETURNING ${
+                    campos[0].campo
+                  } INTO id;  
 
                   select concat_ws('|','REGISTRO INSERTADO CORRECTAMENTE' ,id) into message;
               end if;
@@ -64,7 +61,9 @@ const armarCRUD =(campos,esquema,tabla,cnx)=>{
               end if;
           
               if (operacion='D')then
-                  update ${esquema}.${tabla} set ${campos[campos.length-1].campo} = 0,fecha_modifica=now()::timestamp,usuario_modifica=_usuario_registro
+                  update ${esquema}.${tabla} set ${
+      campos[campos.length - 1].campo
+    } = 0,fecha_modifica=now()::timestamp,usuario_modifica=_usuario_registro
                   where ${campos[0].campo}  = _${campos[0].campo};
               
                   select 'REGISTRO ANULADO CORRECTAMENTE' into message;
@@ -78,46 +77,55 @@ const armarCRUD =(campos,esquema,tabla,cnx)=>{
       $function$
       ;
 
-      `
-      let campos_in = campos.slice(1,-5)
-      let pa_insert = campos.slice(0,-5)
-      // campos_in.push(dts.filter(f=>f.campo == 'usuario_registro')[0])
-      let parametros = ''
-      pa_insert.forEach(e=>{
-          parametros += `_${e.campo} ${e.tipo}, `
-      })
-      let campos_insert = campos.slice(1).map(campos =>campos.campo).join(', ');
+      `;
+    let campos_in = campos.slice(1, -5);
+    let pa_insert = campos.slice(0, -5);
+    // campos_in.push(dts.filter(f=>f.campo == 'usuario_registro')[0])
+    let parametros = "";
+    pa_insert.forEach((e) => {
+      parametros += `_${e.campo} ${e.tipo}, `;
+    });
+    let campos_insert = campos
+      .slice(1)
+      .map((campos) => campos.campo)
+      .join(", ");
 
-      let valores_insert = ''
-      campos_in.forEach(e=>{
-          valores_insert += `${['character varying','text'].includes(e.tipo) ? 'UPPER(':''} _${e.campo} ${['character varying','text'].includes(e.tipo) ? ')':''}, `
-      })
+    let valores_insert = "";
+    campos_in.forEach((e) => {
+      valores_insert += `${
+        ["character varying", "text"].includes(e.tipo) ? "UPPER(" : ""
+      } _${e.campo} ${
+        ["character varying", "text"].includes(e.tipo) ? ")" : ""
+      }, `;
+    });
 
-      let valores_update = ''
-      campos_in.forEach(e=>{
-          valores_update += `${e.campo} = ${['character varying','text'].includes(e.tipo) ? 'UPPER(':''} _${e.campo} ${['character varying','text'].includes(e.tipo) ? ')':''},
-          `
-      })
+    let valores_update = "";
+    campos_in.forEach((e) => {
+      valores_update += `${e.campo} = ${
+        ["character varying", "text"].includes(e.tipo) ? "UPPER(" : ""
+      } _${e.campo} ${
+        ["character varying", "text"].includes(e.tipo) ? ")" : ""
+      },
+          `;
+    });
 
-      plantilla = plantilla.replace('#CAMPOS_IN#',parametros);
-      plantilla = plantilla.replace('#CAMPOS_INSERT#',campos_insert);
-      plantilla = plantilla.replace('#VALORES_INSERT#',valores_insert);
-      plantilla = plantilla.replace('#CAMPOS_UPDATE#',valores_update);
+    plantilla = plantilla.replace("#CAMPOS_IN#", parametros);
+    plantilla = plantilla.replace("#CAMPOS_INSERT#", campos_insert);
+    plantilla = plantilla.replace("#VALORES_INSERT#", valores_insert);
+    plantilla = plantilla.replace("#CAMPOS_UPDATE#", valores_update);
 
-      cnx.obtener(function (er, cn) {
-          cn.query(plantilla, async function (error, resultado) {
-              cn.release();
-              if (error){
-                  console.log('error',error.message);
-                  armado = {template:plantilla,message:error.message}
-                  resolve(armado)
-              } 
-              if (!error){
-                  console.log('se creo todo bien',plantilla);
-                  armado = {template:plantilla,message:'Funcion CRUD creada Satisfactoriamente'}
-                  resolve(armado)
-              } 
-        });
-      });
-  })
-}
+    try {
+      const consulta = await da(plantilla);
+      console.log("se creo todo bien", plantilla);
+      armado = {
+        template: plantilla,
+        message: "Funcion CRUD creada Satisfactoriamente",
+      };
+      resolve(armado);
+    } catch (error) {
+      console.log("error", error.message);
+      armado = { template: plantilla, message: error.message };
+      resolve(armado);
+    }
+  });
+};
