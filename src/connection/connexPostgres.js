2e80b0d3ae9490ff1d72987ea3fe688b26f00698
Cristian Alvarado <codigoworks@gmail.com>
2025-07-21 23:01:25 -0400

    rev

--- src/connection/connexPostgres.js
+++ src/connection/connexPostgres.js
@@ -6,7 +6,7 @@ const config = {
   host: process.env.DB_HOST,
   user: process.env.USER,
   database: process.env.DB,
-  password: process.env.PWD,
+  password: process.env.PWD_POSTG,
   port: process.env.DB_PORT,
   max: 20, 
   idleTimeoutMillis: 30000, 
@@ -18,7 +18,7 @@ export const consulta = async (consulta) =>{
   return new Promise(async (resolve, reject) => {
     let pool = new postgres.Pool(config);
     console.log('CONSULTA==>',consulta);
-    console.log('variables',process.env.PWD,config)
+    console.log('variables',process.env.PWD_POSTG,config)
     let client;
     try {
       client = await pool.connect();
